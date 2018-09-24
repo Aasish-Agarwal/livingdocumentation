4fe47dece49a840f2791ddd8463b92ba4e31a0ab
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2018-09-24 23:42:10 +0530

    updated lottery calc

--- src/main/java/com/infinilearn/simplecalc/SimpleCalculator.java
+++ src/main/java/com/infinilearn/simplecalc/SimpleCalculator.java
@@ -3,20 +3,26 @@ package com.infinilearn.simplecalc;
 public class SimpleCalculator {
     public int sum(int first, int second) 
             throws Exception {
-    	int result = first + second;
-    	if (result > 100 ) 
+    	
+    	// if dividble by 3 return 9
+    
+    	
+    	int result = first*first + second*second;
+    	
+    	if ( first % 3 == 0 ) 
     	{
-    		result = -1;
-    	}
-    	if (result == 88) 
-    	{
-    		result = -1;
+    		result = 9;
     	}
     	
-    	if ( first<0 || second<0) {
-    		result = -1;
+    	if ( first < 0 && second < 0 ) 
+    	{
+    		result = result / 2;
     	}
     	
+    	if ( first < 0  && second > 0) 
+    	{
+    		result = 0;
+    	}
     	return result;
     }
     

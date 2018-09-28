18fa43c53c49c1824e28aa82b059e88e23190fc5
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2018-09-28 12:37:50 +0530

    Updated the calculation logic to handle inputs below 5

--- src/main/java/com/infinilearn/lottery/WinningNumberCalculator.java
+++ src/main/java/com/infinilearn/lottery/WinningNumberCalculator.java
@@ -5,8 +5,7 @@ public class WinningNumberCalculator {
 		int winningamount = seed * seed ;
 		if( seed < 5 ) 
 		{
-			winningamount = (seed * 3 - 7) > 0 ? (seed * 3 - 7) : 0;
-			
+			winningamount = (seed * 3 - 7) > 0 ? (seed * 3 - 7) : 10;
 		} 
 		return winningamount;
 	}

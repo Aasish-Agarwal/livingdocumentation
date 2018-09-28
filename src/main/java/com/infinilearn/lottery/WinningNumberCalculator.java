18eda899926fd741924124a9c681dc0bf9a99355
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2018-09-28 12:27:45 +0530

    added new keywords

--- src/main/java/com/infinilearn/lottery/WinningNumberCalculator.java
+++ src/main/java/com/infinilearn/lottery/WinningNumberCalculator.java
@@ -3,6 +3,11 @@ package com.infinilearn.lottery;
 public class WinningNumberCalculator {
 	public int calcWinningAmount(int seed) {
 		int winningamount = seed * seed ;
+		if( seed < 5 ) 
+		{
+			winningamount = (seed * 3 - 7) > 0 ? (seed * 3 - 7) : 0;
+			
+		} 
 		return winningamount;
 	}
 }

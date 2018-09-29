69e2256f84f6ed445f547c9d3c3e719fde99a41f
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2018-09-29 23:52:48 +0530

    Updated to support Get Time In Text

---
+++ src/main/java/com/infinilearn/rfkeywords/TimeConversionLib.java
@@ -0,0 +1,18 @@
+package com.infinilearn.rfkeywords;
+
+public class TimeConversionLib {
+    /** This means the same instance of this class is used throughout
+     *  the lifecycle of a Robot Framework test execution.
+     */
+    public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";    
+ 
+    /** The Functionality to be tested */
+    public void GetTimeInText (int hour, int minute) 
+    		throws Exception {
+    	
+    	throw new Exception ("Keyword Not Yet Implemented");
+    }
+
+
+
+}
